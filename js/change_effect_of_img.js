337735ed48a82659010b467c402d0d9e989f56e5
e1nur <elnurwebdev@gmail.com>
2025-09-17 22:42:38 +0500

    task8 is completed

---
+++ js/change_effect_of_img.js
@@ -0,0 +1,4 @@
+ function changeEffectOfImage() {
+    document.querySelector('.image-effect__img').style.filter = 'grayscale(0%)';
+    document.querySelector('image-effect__button').disabled = true;
+}
